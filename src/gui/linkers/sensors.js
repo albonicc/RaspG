5ebacbcb504b2720c258dcb76be63f7ece8c0157
albonicc <nikygam@gmail.com>
2020-05-30 01:02:55 -0500

    Now data from sensors.py is printed successfully in the Electron GUI

--- src/gui/linkers/sensors.js
+++ src/gui/linkers/sensors.js
@@ -1,25 +1,36 @@
-function getData(callback){
-    const spawn = require("child_process").spawn;
+function getData(){
+    const {PythonShell} = require("python-shell");
+    const path = require("path");
 
-    const pythonProcess = spawn('python3', ["../../backend/sensors.py"]);
-    var temperature = 'Debugging';
+    let options = {
+        scriptPath: path.join(__dirname, '../../backend')
+    }
 
-    pythonProcess.stdout.on('data', (data) => {
-        mystr = data.toString();
-        myjson = JSON.parse(mystr);
-        // console.log(myjson);
-        // console.log(myjson.temp);
-        // console.log(myjson.air_hum); 
-        // console.log(myjson.soil_hum); 
-        // console.log(myjson.lum);
-        temperature = myjson.temp;    
-    });
-    pythonProcess.on('close', function(code) {
-        return callback(temperature);
-    });
+    var data = new PythonShell('sensors.py', options);
+
+    data.on('message', function(message) {
+
+        myjson = JSON.parse(message);
+        
+        const sensors = document.querySelector('#sensors');
+
+        sensorsTemplate = ` <div id = "temp">
+                            Temperatura: ${myjson.temp}
+                            </div>
+                            <hr/>
+                            Humedad del aire: ${myjson.air_hum}
+                            <hr/>
+                            Humedad de la tierra: ${myjson.soil_hum}
+                            <hr/>
+                            Luminosidad: ${myjson.lum}
+                        `
+        sensors.innerHTML += sensorsTemplate;
+
+    })
+   
 }
-// getData(function(temperature) { document.getElementById("temp").innerHTML += temperature});
-getData(function(temperature) { /* alert(temperature)*/ console.log(temperature)});
+
+getData();
 
 
 
