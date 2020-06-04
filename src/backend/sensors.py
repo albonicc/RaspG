b2b65019c7403cd0b0a8499582d707f4b427dce6
albonicc <nikygam@gmail.com>
2020-06-04 11:43:01 -0500

    Finally fixed sensors.py

--- src/backend/sensors.py
+++ src/backend/sensors.py
@@ -1,13 +1,19 @@
 import sys
 import json
+import time
 
 sensor_values = {
-    "temp": "24 C",
-    "air_hum": "32%",
-    "soil_hum": "88%",
-    "lum": "90%"
-}
+        "temp": 1,
+        "air_hum": 1,
+        "soil_hum": 1,
+        "lum": 1
+    }
 
-print(json.dumps(sensor_values))
-
-sys.stdout.flush()
+while True:
+    print(json.dumps(sensor_values))
+    sensor_values["temp"] += 1
+    sensor_values["air_hum"] += 1
+    sensor_values["soil_hum"] += 1
+    sensor_values["lum"] += 1
+    sys.stdout.flush()
+    time.sleep(1)
