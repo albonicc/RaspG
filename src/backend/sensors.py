bfd18a79c6e68955fb280b5607f6595f6dc4a050
Nicolas Albo <nikygam@gmail.com>
2020-06-05 02:43:03 -0500

    Now Arduino sends successfully data from sensors and it is displayed in the Electron GUI

--- src/backend/sensors.py
+++ src/backend/sensors.py
@@ -6,38 +6,51 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 import subprocess
+from smbus import SMBus
 
-RST = None 
 
-disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
-disp.begin()
-disp.clear()
-disp.display()
+# RST = None 
 
-width = disp.width
-height = disp.height
-image = Image.new('1', (width, height))
+# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+# disp.begin()
+# disp.clear()
+# disp.display()
 
-draw = ImageDraw.Draw(image)
+#width = disp.width
+#height = disp.height
+#image = Image.new('1', (width, height))
 
-draw.rectangle((0,0,width,height), outline=0, fill=0)
+#draw = ImageDraw.Draw(image)
 
+#draw.rectangle((0,0,width,height), outline=0, fill=0)
+'''
 padding = -2
 top = padding
 bottom = height-padding
 x = 0
 font = ImageFont.load_default()
+'''
+bus = SMBus(1);
+address = 0x20
+
+dataF = bus.read_i2c_block_data(address, 0, 4)
+
+air_humid = dataF[0:4]
+air_temp = dataF[4:8]
+hum_dirt = dataF[8:12]
+light = dataF[12:16]
+
+
 
 sensor_values = {
-        "temp": 1,
-        "air_hum": 1,
-        "soil_hum": 1,
-        "lum": 1
+        "temp": air_humid,
+        "air_hum": air_temp,
+        "soil_hum": hum_dirt,
+        "lum": light
     }
 
 while True:
-    print(json.dumps(sensor_values))
-    
+    '''
     # Draw a black filled box to clear the image.
     draw.rectangle((0,0,width,height), outline=0, fill=0)
 
@@ -52,10 +65,48 @@ while True:
     disp.image(image)
     disp.display()
     time.sleep(.1)
+    '''
     
-    sensor_values["temp"] += 1
-    sensor_values["air_hum"] += 1
-    sensor_values["soil_hum"] += 1
-    sensor_values["lum"] += 1
+    dataF = bus.read_i2c_block_data(address, 0, 16)
+    
+    air_humid = dataF[0:4]
+    air_temp = dataF[4:8]
+    hum_dirt = dataF[8:12]
+    light = dataF[12:16]
+    
+    air_humid_blank = []
+    air_temp_blank = []
+    hum_dirt_blank = []
+    light_blank = []
+    
+    air_humid_joined = ""
+    air_temp_joined = ""
+    hum_dirt_joined = ""
+    light_joined = ""
+    
+    for i in range(len(air_humid)):
+        air_humid_blank.append(chr(air_humid[i]))
+    air_humid_joined = air_humid_joined.join(air_humid_blank)
+    for i in range(len(air_temp)):
+        air_temp_blank.append(chr(air_temp[i]))
+    air_temp_joined = air_temp_joined.join(air_temp_blank)
+    for i in range(len(hum_dirt)):
+        hum_dirt_blank.append(chr(hum_dirt[i]))
+    hum_dirt_joined = hum_dirt_joined.join(hum_dirt_blank)
+    for i in range(len(light)):
+        light_blank.append(chr(light[i]))
+    light_joined = light_joined.join(light_blank)
+    sensor_values["temp"] = air_temp_joined
+    sensor_values["air_hum"] = air_humid_joined
+    sensor_values["soil_hum"] = hum_dirt_joined
+    sensor_values["lum"] = light_joined
+    
+    print(json.dumps(sensor_values))
     sys.stdout.flush()
+    
+    air_humid_blank = []
+    air_temp_blank = []
+    hum_dirt_blank = []
+    light_blank = []
+    
     time.sleep(1)
