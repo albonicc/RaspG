74b4f0babafd94628d24fd388c55b4119ec2dfaa
Nicolas Albo <nikygam@gmail.com>
2020-06-05 21:43:10 -0500

    Now sensors data is also display in an OLED screen

--- src/backend/sensors.py
+++ src/backend/sensors.py
@@ -9,27 +9,27 @@ import subprocess
 from smbus import SMBus
 
 
-# RST = None 
+RST = None 
 
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
-# disp.begin()
-# disp.clear()
-# disp.display()
+disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+disp.begin()
+disp.clear()
+disp.display()
 
-#width = disp.width
-#height = disp.height
-#image = Image.new('1', (width, height))
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
 
-#draw = ImageDraw.Draw(image)
+draw = ImageDraw.Draw(image)
 
-#draw.rectangle((0,0,width,height), outline=0, fill=0)
-'''
+draw.rectangle((0,0,width,height), outline=0, fill=0)
+
 padding = -2
 top = padding
 bottom = height-padding
 x = 0
 font = ImageFont.load_default()
-'''
+
 bus = SMBus(1);
 address = 0x20
 
@@ -50,22 +50,22 @@ sensor_values = {
     }
 
 while True:
-    '''
+    
     # Draw a black filled box to clear the image.
     draw.rectangle((0,0,width,height), outline=0, fill=0)
 
     # Write two lines of text.
 
-    draw.text((x, top),       "Temp:              " + str(sensor_values["temp"]),  font=font, fill=255)
-    draw.text((x, top+8),     "Air hum:           " + str(sensor_values["air_hum"]), font=font, fill=255)
-    draw.text((x, top+16),    "soil hum:          " + str(sensor_values["soil_hum"]),  font=font, fill=255)
-    draw.text((x, top+25),    "lum:               " + str(sensor_values["lum"]),  font=font, fill=255)
+    draw.text((x, top),       "Temp:            " + str(sensor_values["temp"]),  font=font, fill=255)
+    draw.text((x, top+8),     "Air hum:         " + str(sensor_values["air_hum"]), font=font, fill=255)
+    draw.text((x, top+16),    "soil hum:        " + str(sensor_values["soil_hum"]),  font=font, fill=255)
+    draw.text((x, top+25),    "lum:             " + str(sensor_values["lum"]),  font=font, fill=255)
 
     # Display image.
     disp.image(image)
     disp.display()
     time.sleep(.1)
-    '''
+    
     
     dataF = bus.read_i2c_block_data(address, 0, 16)
     
